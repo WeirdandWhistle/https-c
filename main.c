f63feeb37a8bb61f239824321d29a82dd167f0fa
wnj <wnj@wnj-server.attlocal.net>
2025-12-07 22:02:27 -0500

    sha256 test

--- main.c
+++ main.c
@@ -11,5 +11,14 @@ int main(){
 		printf("sodium succefuly init\n");
 	}
 
+	unsigned char out[crypto_hash_sha256_BYTES];
+	unsigned char in[] = {1,2,3,4};
+
+	int from = crypto_hash_sha256(out,in,sizeof(in));
+
+	for(int i = 0; i<crypto_hash_sha256_BYTES; i++){
+		printf("%02X",out[i]);
+	}
+
 	return 0;
 }
